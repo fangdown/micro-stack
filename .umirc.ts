29d9b3994bc691519d6ec83d1b4692957e7e507f
fangdown <409951398@qq.com>
2023-12-10 10:28:01 +0800

    fix: 组件路径

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     plugins: ['@umijs/plugins/dist/qiankun'],
     routes: [
         { path: '/', redirect: '/home' },
-        { path: '/home', component: 'home' },
+        { path: '/home', component: './home' },
     ],
     npmClient: 'yarn',
     qiankun: {

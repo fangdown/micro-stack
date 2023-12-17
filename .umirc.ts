e14074753953c2b1c7d70d562211a222f70a3866
fangdown <409951398@qq.com>
2023-12-17 21:47:07 +0800

    fix: 线上本地正常

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'umi';
 import packages from './package.json';
 const publicPathBase = `${packages.name}`;
-
+const isDev = process.env.NODE_ENV === 'development';
+const perfix = isDev ? '' : '/subapp';
 export default defineConfig({
     plugins: ['@umijs/plugins/dist/qiankun'],
     routes: [
@@ -17,7 +18,7 @@ export default defineConfig({
     mountElementId: 'micro-app', //  容器ID
     // 必须要配，不然找不到微应用js
     base: '/' + publicPathBase,
-    cssPublicPath: '/subapp/' + publicPathBase + '/', // 静态资源路径的前缀
-    publicPath: '/subapp/' + publicPathBase + '/', // 静态资源路径的前缀
+    cssPublicPath: perfix + '/' + publicPathBase + '/', // 静态资源路径的前缀
+    publicPath: perfix + '/' + publicPathBase + '/', // 静态资源路径的前缀
     runtimePublicPath: {},
 });

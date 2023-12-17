2b71f62f738724da29c6db08dc60678ab2ddaaa2
fangdown <409951398@qq.com>
2023-12-17 18:53:19 +0800

    fix: 生产模式主子应用正常

--- .umirc.ts
+++ .umirc.ts
@@ -17,8 +17,7 @@ export default defineConfig({
     mountElementId: 'micro-app', //  容器ID
     // 必须要配，不然找不到微应用js
     base: '/' + publicPathBase,
-    cssPublicPath: '/' + publicPathBase + '/', // 静态资源路径的前缀
-    publicPath: '/' + publicPathBase + '/', // 静态资源路径的前缀
-    outputPath: './dist/' + publicPathBase, // 打包输出路径
+    cssPublicPath: '/subapp/' + publicPathBase + '/', // 静态资源路径的前缀
+    publicPath: '/subapp/' + publicPathBase + '/', // 静态资源路径的前缀
     runtimePublicPath: {},
 });
